aa57e981795c092ca81e63594b6724b1f5f852b0
junayednoman <junayednoman05@gmail.com>
2025-11-27 23:08:08 +0600

    fix: project details links updated

--- src/app/components/sections/project details/Banner.tsx
+++ src/app/components/sections/project details/Banner.tsx
@@ -19,36 +19,38 @@ const Banner = ({
       <Container>
         <div
           className="bg-center bg-cover bg-no-repeat rounded-[21px]"
-          style={{ backgroundImage: `url(${image})` }}
-        >
-          <div className="w-full xl:h-[800px] lg:h-[600px] md:h-[500px] sm:h-[350px] h-[300px] rounded-[20px] bg-black-100/30"></div>
+          style={{ backgroundImage: `url(${image})` }}>
+          <div className="w-full xl:h-[800px] lg:h-[600px] md:h-[500px] sm:h-[350px] h-[300px] rounded-[20px] bg-black-100/5"></div>
         </div>
         <div className="flex items-center gap-3 mt-6 lg:px-6">
-          <MagicButtonSmall
-            target="_blank"
-            icon={<FaGithub size={18} />}
-            iconPosition="left"
-            link={github_client}
-          >
-            Client Side
-          </MagicButtonSmall>
+          {github_client !== "#" && (
+            <MagicButtonSmall
+              target="_blank"
+              icon={<FaGithub size={18} />}
+              iconPosition="left"
+              link={github_client}>
+              Client Side
+            </MagicButtonSmall>
+          )}
           {github_server !== "#" && (
             <MagicButtonSmall
               target="_blank"
               icon={<FaGithub size={18} />}
               iconPosition="left"
-              link={github_server}
-            >
-              Server Side
+              link={github_server}>
+              {url === "#" && github_client === "#"
+                ? "Github Repo"
+                : "Server Side"}
             </MagicButtonSmall>
           )}
-          <MagicButtonSmall
-            target="_blank"
-            icon={<FaArrowUpRightFromSquare size={13} />}
-            link={url}
-          >
-            Live Link
-          </MagicButtonSmall>
+          {url !== "#" && (
+            <MagicButtonSmall
+              target="_blank"
+              icon={<FaArrowUpRightFromSquare size={13} />}
+              link={url}>
+              Live Link
+            </MagicButtonSmall>
+          )}
         </div>
       </Container>
     </section>
